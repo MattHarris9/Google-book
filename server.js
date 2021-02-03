980e6e95dcd22c64d318cc9b1a99bf4409203698
Matt Harris <711mattman@gmail.com>
2021-02-03 15:19:42 -0500

    Added the server file

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+}
+
+
+mongoose.connect(process.env.MONGOD_URI || "mongodv://googlebooks", { useNewUrlParser: true });
+
+app.listen(PORT, () => {
+    console.log(`🌎 ==> API server now on port ${PORT}!`);
+  });
