5760c284e4666369c5bdbe92f9caeff2a157e258
Matt Harris <711mattman@gmail.com>
2021-02-07 14:07:53 -0500

    deploy

--- server.js
+++ server.js
@@ -10,9 +10,6 @@ app.use(express.json());
 
 if(process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
-    app.get('*', (request, response) => {
-        response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-    });
 }
 
 
