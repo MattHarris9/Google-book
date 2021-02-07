8e640901e4260aba6cf692c0f47a9fbce6f4e550
Matt Harris <711mattman@gmail.com>
2021-02-06 19:56:51 -0500

    Started making the components folder

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,15 +2,23 @@ import axios from "axios";
 
 export default {
     getBook: function (query) {
-        return axios.get("https://www.googleapis.com/books/v1/volumes/volumeId")
+        return axios.get(`https://www.googleapis.com/books/v1/volumes/volumes?q=${query}`)
     },
-    deleteBook: function(id) {
-        return axios.delete("/api/books/" + id).then(result => result.data);
-    },
-    saveBook: function (bookData) {
-        return axios.post("/api/books", bookData).then(result => result.data);    
-    },
-    savedBooks: function() {
-        return axios.get("/api/books").then(result => result.data);
-    }
+   // Saves book to the DB
+   saveBook: function(bookData) {
+    return axios.post("/api/books", bookData);
+},
+// Gets saved books from DB
+getBooks: function() {
+    return axios.get("/api/books");
+},
+// Gets book with the given ID
+getBook: function(id) {
+    return axios.get("/api/books/" + id);
+},
+// Deletes book with the given ID
+deleteBook: function(id) {
+    return axios.delete("/api/books/" + id);
+}
+
 };
